bb0fdac70b1d65221ea4a436badc8ac21628bc40
Sun <sun@runners.im>
2024-06-12 22:36:01 +0900

    수정: Git 주소 수정

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nhncloud_email',
+    name='nhncloud-email',
     version='0.1.3',
     packages=find_packages(),
     install_requires=[
@@ -11,7 +11,7 @@ setup(
     description='A Python library to send Email using NHN Cloud API',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/RUNNERS-IM/python-nhn-cloud-email",
+    url="https://github.com/RUNNERS-IM/python-nhncloud-email",
     author='RUNNERS',
     author_email="dev@runners.im",
     classifiers=[
